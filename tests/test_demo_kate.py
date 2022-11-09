d7d45fcb9dd5a317ad266d17560be4a57979b6a3
Keldieva <56282112+Keldieva@users.noreply.github.com>
2022-11-09 16:24:48 -0500

    new updates to setup and first test

--- tests/test_demo_kate.py
+++ tests/test_demo_kate.py
@@ -1,19 +1,58 @@
 from selenium.webdriver.common.by import By
-import pytest
-
+import unittest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
+user_name = 'standard_user'
+password = 'secret_sauce'
 
-def test_kate():
-    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    browser.get('https://www.selenium.dev/downloads')
-    browser.find_element(By.XPATH, "//a[contains(@href,'https://github.com/SeleniumHQ/')]").click()
-    assert browser.title in 'Selenium'
-    browser.quit()
+class BaseTest(unittest.TestCase):
+    def setUp(self, url=None):
+        service = Service(ChromeDriverManager().install())
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--window-size=1920,1080')
+        self.driver = webdriver.Chrome(service=service, options=chrome_options)
+        self.driver.get('https://www.saucedemo.com/')
+
+    def tearDown(self):
+        self.driver.quit()
+
+class BasePage(BaseTest):
+    def test_sausedemo_tc002_00_01(self):
+        #verifyin I am on a right page with a right title
+        self.assertEqual('Swag Labs', self.driver.title)
+
+        #login as an authorithed user
+        self.driver.find_element(By.ID, 'user-name').send_keys(user_name)
+        self.driver.find_element(By.ID, 'password').send_keys(password)
+        self.driver.find_element(By.ID, 'login-button').click()
+
+        #verifying I am on a right link
+        self.assertEqual('https://www.saucedemo.com/inventory.html', self.driver.current_url)
+
+        #clicking on a add to cart button
+        self.driver.find_element(By.ID, 'add-to-cart-sauce-labs-backpack').click()
+        count_item_1 = self.driver.find_element(By.CLASS_NAME, 'shopping_cart_badge').text
+
+        #make sure cart is counting right sum of items every click
+        self.assertEqual('1', count_item_1)
+
+        #second_ and third click to add another items to cart
+        self.driver.find_element(By.ID, 'add-to-cart-sauce-labs-bike-light').click()
+        self.driver.find_element(By.ID, 'add-to-cart-sauce-labs-onesie').click()
+
+        # make sure cart is counting right sum of items every click
+        count_item_3 = self.driver.find_element(By.CLASS_NAME, 'shopping_cart_badge').text
+        self.assertEqual('3', count_item_3)
 
 
-# from string import printable
-#
-# print(printable)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
