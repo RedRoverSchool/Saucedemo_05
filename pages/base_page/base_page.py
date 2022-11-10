5a50da7e29777f013ec38de3f4f823b63cc1bbe9
Alex <alexander.keepsolid@gmail.com>
2022-11-10 20:16:54 +0200

    basepage methods added

--- pages/base_page/base_page.py
+++ pages/base_page/base_page.py
@@ -1,3 +1,4 @@
+import time
 from typing import List
 
 from selenium.common import TimeoutException
@@ -68,7 +69,7 @@ class BasePage:
     def highlight_web_element(self, locator) -> None:
         element = self.element_is_visible(locator)
         self.browser.execute_script(
-            "arguments[0].style.border='3px ridge #33ffff'", element
+            "arguments[0].style.border='3px solid red'", element
         )
 
     def double_click(self, locator) -> WebElement:
@@ -151,7 +152,9 @@ class BasePage:
 
     def is_text_in_value(self, locator, value):
         try:
-            result = self.wait.until(EC.text_to_be_present_in_element_value(locator, value))
+            result = self.wait.until(
+                EC.text_to_be_present_in_element_value(locator, value)
+            )
         except TimeoutException:
             return False
         else:
@@ -170,7 +173,9 @@ class BasePage:
         return result
 
     def is_selected_be(self, locator, selected=True):
-        result = self.wait.until(EC.element_located_selection_state_to_be(locator, selected))
+        result = self.wait.until(
+            EC.element_located_selection_state_to_be(locator, selected)
+        )
         return result
 
     def is_alert_present(self):
@@ -185,3 +190,117 @@ class BasePage:
         alert = self.browser.switch_to.alert
         return alert.text()
 
+    def refresh_page(self):
+        self.browser.refresh()
+
+    def scroll_down(self, offset=0):
+        if offset:
+            self.browser.execute_script("window.scrollTo(0, {0});".format(offset))
+        else:
+            self.browser.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);"
+            )
+
+    def scroll_up(self, offset=0):
+        if offset:
+            self.browser.execute_script("window.scrollTo(0, -{0});".format(offset))
+        else:
+            self.browser.execute_script(
+                "window.scrollTo(0, -document.body.scrollHeight);"
+            )
+
+    def switch_to_iframe(self, iframe):
+        self.browser.switch_to.frame(iframe)
+
+    def switch_out_iframe(self):
+        self.browser.switch_to.default_content()
+
+    def get_current_url(self):
+        return self.browser.current_url
+
+    def get_page_source(self):
+        source = ""
+        try:
+            source = self.browser.page_source
+        except:
+            return None
+        return source
+
+    def check_js_errors(self, ignore_list=None):
+        ignore_list = ignore_list or []
+        logs = self.browser.get_log("browser")
+        for log_message in logs:
+            if log_message["level"] != "WARNING":
+                ignore = False
+                for issue in ignore_list:
+                    if issue in log_message["message"]:
+                        ignore = True
+                        break
+                assert ignore, f'JS error "{log_message}" on the page!'
+
+    def wait_page_loaded(
+        self,
+        timeout=60,
+        check_js_complete=True,
+        check_page_changes=False,
+        check_images=False,
+        wait_for_element=None,
+        wait_element_to_disappear="",
+        sleep_time=2,
+    ):
+        page_loaded = False
+        double_check = False
+        k = 0
+        if sleep_time:
+            time.sleep(sleep_time)
+        source = ""
+        try:
+            source = self.browser.page_source
+        except:
+            pass
+        # Wait until page loaded (and scroll it, to make sure all objects will be loaded):
+        while not page_loaded:
+            time.sleep(0.5)
+            k += 1
+            if check_js_complete:
+                # Scroll down and wait when page will be loaded:
+                try:
+                    self.browser.execute_script(
+                        "window.scrollTo(0, document.body.scrollHeight);"
+                    )
+                    page_loaded = self.browser.execute_script(
+                        "return document.readyState == 'complete';"
+                    )
+                except Exception as e:
+                    pass
+            if page_loaded and check_page_changes:
+                # Check if the page source was changed
+                new_source = ""
+                try:
+                    new_source = self.browser.page_source
+                except:
+                    pass
+                page_loaded = new_source == source
+                source = new_source
+            # Wait when some element will disappear:
+            if page_loaded and wait_for_xpath_to_disappear:
+                bad_element = None
+                try:
+                    bad_element = WebDriverWait(self.browser, 0.1).until(
+                        EC.presence_of_element_located((wait_element_to_disappear))
+                    )
+                except:
+                    pass  # Ignore timeout errors
+                page_loaded = not bad_element
+            if page_loaded and wait_for_element:
+                try:
+                    page_loaded = WebDriverWait(self._web_driver, 0.1).until(
+                        EC.element_to_be_clickable(wait_for_element._locator)
+                    )
+                except:
+                    pass  # Ignore timeout errors
+            assert k < timeout, "The page loaded more than {0} seconds!".format(timeout)
+            if page_loaded and not double_check:
+                page_loaded = False
+                double_check = True
+        self.browser.execute_script("window.scrollTo(document.body.scrollHeight, 0);")
