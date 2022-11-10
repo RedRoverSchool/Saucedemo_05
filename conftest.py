5e604908e6fdc46a5b51d56ebf8e27e40a6f5a62
Keldieva <56282112+Keldieva@users.noreply.github.com>
2022-11-10 14:30:36 -0500

    added conftest file and use some fixtures on test_demo_kate

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = Options()
+options.add_argument("start-maximized")
+options.add_argument("--headless")
+
+
+@pytest.fixture(scope="class")
+def driver_init(request):
+    chrome_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    request.cls.driver = chrome_driver
+    yield
+    chrome_driver.close()
