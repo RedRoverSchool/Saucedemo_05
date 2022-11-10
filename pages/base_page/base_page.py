7c1221317301351ca06831379d211dbcf9636271
Alex <alexander.keepsolid@gmail.com>
2022-11-10 20:25:14 +0200

    basepage methods commented

--- pages/base_page/base_page.py
+++ pages/base_page/base_page.py
@@ -238,69 +238,69 @@ class BasePage:
                         break
                 assert ignore, f'JS error "{log_message}" on the page!'
 
-    def wait_page_loaded(
-        self,
-        timeout=60,
-        check_js_complete=True,
-        check_page_changes=False,
-        check_images=False,
-        wait_for_element=None,
-        wait_element_to_disappear="",
-        sleep_time=2,
-    ):
-        page_loaded = False
-        double_check = False
-        k = 0
-        if sleep_time:
-            time.sleep(sleep_time)
-        source = ""
-        try:
-            source = self.browser.page_source
-        except:
-            pass
-        # Wait until page loaded (and scroll it, to make sure all objects will be loaded):
-        while not page_loaded:
-            time.sleep(0.5)
-            k += 1
-            if check_js_complete:
-                # Scroll down and wait when page will be loaded:
-                try:
-                    self.browser.execute_script(
-                        "window.scrollTo(0, document.body.scrollHeight);"
-                    )
-                    page_loaded = self.browser.execute_script(
-                        "return document.readyState == 'complete';"
-                    )
-                except Exception as e:
-                    pass
-            if page_loaded and check_page_changes:
-                # Check if the page source was changed
-                new_source = ""
-                try:
-                    new_source = self.browser.page_source
-                except:
-                    pass
-                page_loaded = new_source == source
-                source = new_source
-            # Wait when some element will disappear:
-            if page_loaded and wait_for_xpath_to_disappear:
-                bad_element = None
-                try:
-                    bad_element = WebDriverWait(self.browser, 0.1).until(
-                        EC.presence_of_element_located((wait_element_to_disappear))
-                    )
-                except:
-                    pass  # Ignore timeout errors
-                page_loaded = not bad_element
-            if page_loaded and wait_for_element:
-                try:
-                    page_loaded = WebDriverWait(self._web_driver, 0.1).until(
-                        EC.element_to_be_clickable(wait_for_element._locator)
-                    )
-                except:
-                    pass  # Ignore timeout errors
-            assert k < timeout, "The page loaded more than {0} seconds!".format(timeout)
-            if page_loaded and not double_check:
-                page_loaded = False
-                double_check = True
-        self.browser.execute_script("window.scrollTo(document.body.scrollHeight, 0);")
+    # def wait_page_loaded(
+    #     self,
+    #     timeout=60,
+    #     check_js_complete=True,
+    #     check_page_changes=False,
+    #     check_images=False,
+    #     wait_for_element=None,
+    #     wait_element_to_disappear="",
+    #     sleep_time=2,
+    # ):
+    #     page_loaded = False
+    #     double_check = False
+    #     k = 0
+    #     if sleep_time:
+    #         time.sleep(sleep_time)
+    #     source = ""
+    #     try:
+    #         source = self.browser.page_source
+    #     except:
+    #         pass
+    #     # Wait until page loaded (and scroll it, to make sure all objects will be loaded):
+    #     while not page_loaded:
+    #         time.sleep(0.5)
+    #         k += 1
+    #         if check_js_complete:
+    #             # Scroll down and wait when page will be loaded:
+    #             try:
+    #                 self.browser.execute_script(
+    #                     "window.scrollTo(0, document.body.scrollHeight);"
+    #                 )
+    #                 page_loaded = self.browser.execute_script(
+    #                     "return document.readyState == 'complete';"
+    #                 )
+    #             except Exception as e:
+    #                 pass
+    #         if page_loaded and check_page_changes:
+    #             # Check if the page source was changed
+    #             new_source = ""
+    #             try:
+    #                 new_source = self.browser.page_source
+    #             except:
+    #                 pass
+    #             page_loaded = new_source == source
+    #             source = new_source
+    #         # Wait when some element will disappear:
+    #         if page_loaded and wait_for_xpath_to_disappear:
+    #             bad_element = None
+    #             try:
+    #                 bad_element = WebDriverWait(self.browser, 0.1).until(
+    #                     EC.presence_of_element_located((wait_element_to_disappear))
+    #                 )
+    #             except:
+    #                 pass  # Ignore timeout errors
+    #             page_loaded = not bad_element
+    #         if page_loaded and wait_for_element:
+    #             try:
+    #                 page_loaded = WebDriverWait(self.browser, 0.1).until(
+    #                     EC.element_to_be_clickable(wait_for_element._locator)
+    #                 )
+    #             except:
+    #                 pass  # Ignore timeout errors
+    #         assert k < timeout, "The page loaded more than {0} seconds!".format(timeout)
+    #         if page_loaded and not double_check:
+    #             page_loaded = False
+    #             double_check = True
+    #     self.browser.execute_script("window.scrollTo(document.body.scrollHeight, 0);")
