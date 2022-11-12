e2b2a9fdbaba3050bef487505c99da79ec6263b4
Alex <alexander.keepsolid@gmail.com>
2022-11-12 13:43:44 +0200

    Sorting tests refactored

--- tests/test_inventory_page_p1.py
+++ tests/test_inventory_page_p1.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 import allure
 from pages.inventory_page.inventory_page import InventoryPage
@@ -5,35 +7,9 @@ from pages.inventory_page.inventory_page import InventoryPage
 LOGIN_PAGE_URL = "https://www.saucedemo.com/"
 INVENTORY_URL = "https://www.saucedemo.com/inventory.html"
 
-sorting_price_cases = (
-    ("Price (low to high)", ("$7.99", "$9.99", "$15.99", "$15.99", "$29.99", "$49.99")),
-    ("Price (high to low)", ("$49.99", "$29.99", "$15.99", "$15.99", "$9.99", "$7.99")),
-)
+sorting_price_cases = ("Price (low to high)", "Price (high to low)")
 
-sorting_name_cases = (
-    (
-        "Name (Z to A)",
-        (
-            "Test.allTheThings() T-Shirt (Red)",
-            "Sauce Labs Onesie",
-            "Sauce Labs Fleece Jacket",
-            "Sauce Labs Bolt T-Shirt",
-            "Sauce Labs Bike Light",
-            "Sauce Labs Backpack",
-        ),
-    ),
-    (
-        "Name (A to Z)",
-        (
-            "Sauce Labs Backpack",
-            "Sauce Labs Bike Light",
-            "Sauce Labs Bolt T-Shirt",
-            "Sauce Labs Fleece Jacket",
-            "Sauce Labs Onesie",
-            "Test.allTheThings() T-Shirt (Red)",
-        ),
-    ),
-)
+sorting_name_cases = ("Name (Z to A)", "Name (A to Z)")
 
 
 class TestInventoryPage:
@@ -59,26 +35,30 @@ class TestInventoryPage:
 
     @allure.epic("Inventory Page Test")
     @allure.story("TC_002.00.02.01 | Test Inventory Page Sorting by price")
-    @pytest.mark.parametrize("sorting, prices", sorting_price_cases)
-    def test_inventory_page_sorting_by_price(self, browser, sorting, prices):
+    @pytest.mark.parametrize("sorting", sorting_price_cases)
+    def test_inventory_page_sorting_by_price(self, browser, sorting):
         page = InventoryPage(browser, url=LOGIN_PAGE_URL)
         page.open()
         page.login_standard_user()
         page.set_sorting_order(sorting)
         inventory_items = page.find_items_cards()
-        assert page.extract_items_prices(inventory_items) == list(
-            prices
-        ), "Sorting by price works incorrect"
+        prices_list = page.extract_items_prices(inventory_items)
+        expected_list = sorted(
+            prices_list, reverse=True if sorting == "Price (high to low)" else False
+        )
+        assert expected_list == prices_list, "Sorting by price works incorrect"
 
     @allure.epic("Inventory Page Test")
     @allure.story("TC_002.00.02.02 | Test Inventory Page Sorting by name")
-    @pytest.mark.parametrize("sorting, names", sorting_name_cases)
-    def test_inventory_page_sorting_by_name(self, browser, sorting, names):
+    @pytest.mark.parametrize("sorting", sorting_name_cases)
+    def test_inventory_page_sorting_by_name(self, browser, sorting):
         page = InventoryPage(browser, url=LOGIN_PAGE_URL)
         page.open()
         page.login_standard_user()
         page.set_sorting_order(sorting)
         inventory_items = page.find_items_cards()
-        assert page.extract_items_names(inventory_items) == list(
-            names
-        ), "Sorting by name works incorrect"
+        names_list = page.extract_items_names(inventory_items)
+        expected_list = sorted(
+            names_list, reverse=True if sorting == "Name (Z to A)" else False
+        )
+        assert expected_list == names_list, "Sorting by name works incorrect"
