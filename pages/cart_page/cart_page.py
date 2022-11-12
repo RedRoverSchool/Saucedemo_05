baa751fd717f9c17adbe244bcb7da10f218100d3
Alex <alexander.keepsolid@gmail.com>
2022-11-12 22:33:26 +0200

    some fixes

--- pages/cart_page/cart_page.py
+++ pages/cart_page/cart_page.py
@@ -3,7 +3,7 @@ from typing import List
 from pages.base_page.base_page import BasePage
 from pages.login_page.login_page import LoginPage
 from pages.inventory_page.inventory_page import InventoryPage
-from pages.inventory_page.inventory_page_locators import InventoryPageLocators
+from pages.cart_page.cart_page_locators import CartPageLocators
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 
