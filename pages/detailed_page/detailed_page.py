b0d82a945ccd692efd0b1391f9e484c5d386cfe1
LENOVOTT\tatya <t.trushlyakova@gmail.com>
2022-11-12 10:10:24 -0800

    added detailed page, changed some locators on inventory page

--- pages/detailed_page/detailed_page.py
+++ pages/detailed_page/detailed_page.py
@@ -1,12 +1,14 @@
 from pages.base_page.base_page import BasePage
 from pages.login_page.login_page import LoginPage
 from pages.detailed_page.detailed_page_locator import DetailedPageLocators
+from pages.inventory_page.inventory_page_locators import InventoryPageLocators
+
 
 
 class DetailedPage(LoginPage, BasePage):
 
     def open_first_item(self):                                  #кликнуть первый элемент в списке
-        self.click_button(DetailedPageLocators.ITEM_TITLE)
+        self.click_button(InventoryPageLocators.ITEMS_CARDS)
 
     def click_add_to_cart(self):                                #кликнуть Add to cart на странице продукта
         self.click_button(DetailedPageLocators.ADD_TO_CART_BUTTON)
