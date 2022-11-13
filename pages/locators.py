0b0dbbb5783fab6142022af3c72aa95728b263b7
RustamSu <surstmal@gmail.com>
2022-11-13 14:57:47 -0500

    fixes

--- pages/locators.py
+++ pages/locators.py
@@ -35,8 +35,14 @@ class ProductPageLocators:
     INVENTORY_ITEM_PRICE = (By.CSS_SELECTOR, ".inventory_item_price")
     INVENTORY_ITEM_LINK = (By.CSS_SELECTOR, ".inventory_item_img .inventory_item_img")
 
-    SAUCE_LABS_BACKPACK_ADD_TO_CART = (By.CSS_SELECTOR, "#add-to-cart-sauce-labs-backpack")
-    SAUCE_LABS_BACKPACK_REMOVE_FROM_CART = (By.CSS_SELECTOR, "#remove-sauce-labs-backpack")
+    SAUCE_LABS_BACKPACK_ADD_TO_CART = (
+        By.CSS_SELECTOR,
+        "#add-to-cart-sauce-labs-backpack",
+    )
+    SAUCE_LABS_BACKPACK_REMOVE_FROM_CART = (
+        By.CSS_SELECTOR,
+        "#remove-sauce-labs-backpack",
+    )
     SHOPPING_CART_LINK = (By.CSS_SELECTOR, ".shopping_cart_link")
     SORT_DROPDOWN = (By.CLASS_NAME, "select_container")
     PRICE_LOW_TO_HIGH = (By.CSS_SELECTOR, "[value='lohi']")
