d5e9a5442d9e03502eab5dc93ec746974303aab1
Alex <alexander.keepsolid@gmail.com>
2022-11-18 23:27:48 +0200

    "browser": "Chrome"

--- conf/browser_config.py
+++ conf/browser_config.py
@@ -1,5 +1,5 @@
 browser_config = {
-    "browser": "Firefox",  # Firefox, Edge, Chrome
+    "browser": "Chrome",  # Firefox, Edge, Chrome
     "headless": True,
     "proxy_server": "",
     "user_agent": "",
