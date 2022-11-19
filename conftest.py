0c2a4b05a496c0d2afce0097c9b5973c12ec6df4
Keldieva <56282112+Keldieva@users.noreply.github.com>
2022-11-18 22:16:20 -0500

    added a new command to sh file and changed conftest

--- conftest.py
+++ conftest.py
@@ -1,20 +1,86 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.edge.options import Options as EdgeOptions
+from selenium.webdriver.edge.service import Service as EdgeService
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+# from selenium.webdriver.safari.options import Options as SafariOptions
+# from selenium.webdriver.safari.service import Service as SafariService
+import conf
 
 
-@pytest.fixture(params=['chrome', 'safari'], autouse=True)
-def driver(request):
-    if request.param == 'chrome':
-        driver = webdriver.Chrome(
-            service=Service(ChromeDriverManager().install()), options=Options()
+@pytest.fixture(scope='class')
+def driver(browser):
+    if browser == 'chrome':
+        chrome_option = ChromeOptions()
+        chrome_option.headless = conf.BROWSER_HEADLESS
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_option
+                                  )
+    elif browser == 'firefox':
+        gecko_option = FirefoxOptions()
+        gecko_option.headless = conf.BROWSER_HEADLESS
+        driver = webdriver.Firefox(
+            service=FirefoxService(GeckoDriverManager().install()), options=gecko_option
+        )
+    elif browser == 'safari':
+        driver = webdriver.Safari()
+        driver.headless = conf.BROWSER_HEADLESS
+
+    elif browser == 'edge':
+        edge_options = EdgeOptions()
+        driver = webdriver.Edge(
+            service=EdgeService(EdgeChromiumDriverManager().install()),
+            options=edge_options,
         )
     else:
-        driver = webdriver.Safari()
+        raise Exception("Wrong browser")
 
+    return driver
+
+
+@pytest.fixture(scope='class', autouse=True)
+def tear_down(driver):
+    print('\n*** start fixture = setup ***\n')
     driver.get('https://www.saucedemo.com/')
-    yield driver
+    yield
     driver.quit()
+    print('\n*** end fixture = teardown ***\n')
 
+
+@pytest.fixture(scope='class', autouse=True)
+def browser(request):
+    return request.config.getoption("--browser")
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+
+
+    # parser.addoption(
+    #     url="--url",
+    #     default="'https://www.saucedemo.com/'",
+    #     help="define url: https://www.saucedemo.com/"
+    # )
+#
+# @pytest.fixture(scope='class')
+# def url(request):
+#     return request.config.getoption("--url")
+#
+# fixture for crossbrowser testing
+# @pytest.fixture(params=['chrome', 'safari'], autouse=True)
+# def driver(request):
+#     if request.param == 'chrome':
+#         driver = webdriver.Chrome(
+#             service=Service(ChromeDriverManager().install()), options=Options()
+#         )
+#     else:
+#         driver = webdriver.Safari()
+#
+#     driver.get('https://www.saucedemo.com/')
+#     yield driver
+#     driver.quit()
--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -Rf ./allure-report
-poetry run pytest -s -v --alluredir=./allure-report
+poetry run pytest -s -v --browser=safari --alluredir=./allure-report
 allure serve ./allure-report
