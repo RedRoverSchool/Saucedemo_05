6bb2170ac25182fb829195dc2a9c0ca86cfca571
Lipatnikova <106307423+Lipatnikova@users.noreply.github.com>
2022-11-29 20:42:59 +0400

    add tests Your_info invalid data

--- tests/test_your_info_page.py
+++ tests/test_your_info_page.py
@@ -2,7 +2,6 @@ import allure
 import random
 from conf.website_config import WebSiteConfig
 from pages.inventory_page.inventory_page_locators import InventoryPageLocators
-from pages.cart_page.cart_page import CartPage
 from pages.your_info_page.your_info_page import YourInfoPage
 from pages.cart_page.cart_page_locators import CartPageLocators
 from pages.your_info_page.your_info_page_locators import YourInfoLocators
@@ -98,3 +97,51 @@ class TestYourInfoPage:
         page.click_button(CartPageLocators.BTN_CHECKOUT)
         page.click_continue()
         assert page.get_error_msg_text() == "Error: First Name is required"
+
+    @allure.epic("Checkout: Your Information Page Test")
+    @allure.story("TC_005.00.02 First Name is empty")
+    def test_checkout_fields_are_empty(self, browser):
+
+        page = self.open_page(browser)
+        inventory_items = page.find_items_cards()
+        randon_num = random.randint(1, WebSiteConfig.ITEMS_COUNTER)
+        for item in random.sample(inventory_items, randon_num):
+            page.click_item_cart_button(item)
+        page.click_cart_button()
+        page.click_button(CartPageLocators.BTN_CHECKOUT)
+        page.send_keys_to_input(YourInfoLocators.INPUT_LAST_NAME, page.random_word())
+        page.send_keys_to_input(YourInfoLocators.INPUT_ZIP_CODE, page.random_word())
+        page.click_continue()
+        assert page.get_error_msg_text() == "Error: First Name is required"
+
+    @allure.epic("Checkout: Your Information Page Test")
+    @allure.story("TC_005.00.02 Last Name is empty")
+    def test_checkout_fields_are_empty(self, browser):
+
+        page = self.open_page(browser)
+        inventory_items = page.find_items_cards()
+        randon_num = random.randint(1, WebSiteConfig.ITEMS_COUNTER)
+        for item in random.sample(inventory_items, randon_num):
+            page.click_item_cart_button(item)
+        page.click_cart_button()
+        page.click_button(CartPageLocators.BTN_CHECKOUT)
+        page.send_keys_to_input(YourInfoLocators.INPUT_FIRST_NAME, page.random_word())
+        page.send_keys_to_input(YourInfoLocators.INPUT_ZIP_CODE, page.random_word())
+        page.click_continue()
+        assert page.get_error_msg_text() == "Error: Last Name is required"
+
+    @allure.epic("Checkout: Your Information Page Test")
+    @allure.story("TC_005.00.02 Zip/Postal Code is empty")
+    def test_checkout_fields_are_empty(self, browser):
+
+        page = self.open_page(browser)
+        inventory_items = page.find_items_cards()
+        randon_num = random.randint(1, WebSiteConfig.ITEMS_COUNTER)
+        for item in random.sample(inventory_items, randon_num):
+            page.click_item_cart_button(item)
+        page.click_cart_button()
+        page.click_button(CartPageLocators.BTN_CHECKOUT)
+        page.send_keys_to_input(YourInfoLocators.INPUT_FIRST_NAME, page.random_word())
+        page.send_keys_to_input(YourInfoLocators.INPUT_LAST_NAME, page.random_word())
+        page.click_continue()
+        assert page.get_error_msg_text() == "Error: Postal Code is required"
